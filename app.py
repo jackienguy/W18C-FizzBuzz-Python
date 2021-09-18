c31426b010eb017ed66490f93c599fbd675c802a
Your Name <you@example.com>
2021-09-18 14:34:47 -0600

    final commit completed code

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 def divisable_nums(number_array):
     for number in number_array:
         if ((number % 3 == 0) and (number % 5 == 0)):
+            print(number)
             print("BuzzFizz")
         elif (number % 5 == 0):
+            print(number)
             print("Buzz")
         elif (number % 3 == 0):
+            print(number)
             print("Fizz")
         else:
+            print(number)
             print("Not divisible by 3 or 5")
 
 numbers = [15, 12, 32, 38, 45, 85, 83, 19, 100, 71, 48, 18, 30, 52, 60]
